d9a954a25ae31f8e8c5db2ec92628121d0a7e960
Marciliano Chavez <marcilianochaveziii@gmail.com>
2023-12-04 21:00:54 +0800

    Add project files.

---
+++ TigerPadProj/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TigerPadProj.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View("SignUp");
+        }
+        public IActionResult UserLogin()
+        {
+            return View("UserLogin");
+        }
+
+        public IActionResult AdminLogin()
+        {
+            return View("AdminLogin");
+        }
+
+        public IActionResult About()
+        {
+            return View("About");
+        }
+    }
+}
---
+++ TigerPadProj/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TigerPadProj.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult UserHomepage()
+        {
+            return View("UserHomepage");
+        }
+
+        public IActionResult UserMyPosts()
+        {
+            return View("UserMyPosts");
+        }
+
+        public IActionResult UserFollowing()
+        {
+            return View("UserFollowing");
+        }
+    }
+}
