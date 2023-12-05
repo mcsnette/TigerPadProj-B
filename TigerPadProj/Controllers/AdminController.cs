c0843ff451a0c316bd117af24cf09668720828ad
Marciliano Chavez <marcilianochaveziii@gmail.com>
2023-12-05 16:22:45 +0800

    All ADMIN Pages

---
+++ TigerPadProj/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TigerPadProj.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult AdminHomepage()
+        {
+            return View("AdminHomepage");
+        }
+
+        public IActionResult AdminMyPosts()
+        {
+            return View("AdminMyPosts");
+        }
+
+        public IActionResult AdminFollowing()
+        {
+            return View("AdminFollowing");
+        }
+
+        public IActionResult AdminAllPosts()
+        {
+            return View("AdminAllPosts");
+        }
+
+        public IActionResult AdminAllAccounts()
+        {
+            return View("AdminAllAccounts");
+        }
+    }
+}
--- TigerPadProj/wwwroot/js/script.js
+++ TigerPadProj/wwwroot/js/script.js
@@ -39,4 +39,64 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    var deletePostButtons = document.querySelectorAll('.delete-post-btn');
+    var deletePostModal = document.getElementById('deletePostModal');
+    var closeModalButton = document.querySelector('.no-post-btn');
 
+    deletePostButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
+            deletePostModal.style.display = 'block';
+        });
+    });
+
+    closeModalButton.addEventListener('click', function () {
+        deletePostModal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === deletePostModal) {
+            deletePostModal.style.display = 'none';
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var createModal = document.getElementById('createModal');
+    var leftButton = document.querySelector('.admin-left-button');
+    var closeModalButton = document.getElementById('closeModalButton');
+
+    leftButton.addEventListener('click', function () {
+        createModal.style.display = 'block';
+    });
+
+    closeModalButton.addEventListener('click', function () {
+        createModal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === createModal) {
+            createModal.style.display = 'none';
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var profileModal = document.getElementById('profileModal');
+    var leftProfile = document.querySelector('.admin-left-profile');
+    var closeModalButton = document.getElementsByClassName('cancel-btn')[0];
+
+    leftProfile.addEventListener('click', function () {
+        profileModal.style.display = 'block';
+    });
+
+    closeModalButton.addEventListener('click', function () {
+        profileModal.style.display = 'none';
+    });
+
+    window.addEventListener('click', function (event) {
+        if (event.target === profileModal) {
+            profileModal.style.display = 'none';
+        }
+    });
+});
